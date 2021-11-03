197a60f4ceaf50241a12227302ac28e7812b28d6
fawaz bayureksa <fawwazbayureksa@gmail.com>
2021-11-03 16:35:24 +0800

    Commit second

---
+++ resources/views/emails/posts.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Belajar Laravel - Email</title>
+</head>
+<body>
+        <h4>  Hi saya {{$name}}  </h4>
+        <p>Saya baru saja {{$title}}</p>
+        
+</body>
+</html>
